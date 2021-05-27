2fc30f5a016dc571c7d97db1dd0118ef47ceaf61
Shafiaa21 <bsef18m030@pucit.edu.pk>
2021-05-27 05:19:43 -0700

    a

--- Fragment/app/src/main/java/com/example/fragment/MainActivity.java
+++ Fragment/app/src/main/java/com/example/fragment/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button Button1, Button2, Button3;
+    LinearLayout myLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        
     }
 }
